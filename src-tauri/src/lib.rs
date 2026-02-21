0d5fa459e9e00a682e07e35c93b77d2fbd588b85
Ayaan <develop.ayaan@gmail.com>
2026-02-22 03:40:54 +0530

    refactor: removed dead code / file

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,7 +18,6 @@ use commands::*;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_autostart::init(tauri_plugin_autostart::MacosLauncher::LaunchAgent, None))
         .setup(|app| {
             let mut commands = HashMap::new();
